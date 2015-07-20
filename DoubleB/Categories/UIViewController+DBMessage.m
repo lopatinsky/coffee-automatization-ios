16a995459d57f5ada185359edf13c41677adb084
yabalaban <balaban.alexander@gmail.com>
2015-07-20 13:34:49 +0300

    company mail

--- DoubleB/Categories/UIViewController+DBMessage.m
+++ DoubleB/Categories/UIViewController+DBMessage.m
@@ -7,16 +7,25 @@
 //
 
 #import "UIViewController+DBMessage.h"
+#import "DBAPIClient.h"
 
 @implementation UIViewController (DBMessage)
 
 static void (^dbMailViewControllerCallBack)(BOOL completed);
 
-- (void)presentMailViewControllerWithRecipients:(NSArray *)recipients callback:(void(^)(BOOL completed))callback{
-    if([MFMailComposeViewController canSendMail]){
+- (void)presentMailViewControllerWithRecipients:(NSArray *)recipients callback:(void(^)(BOOL completed))callback {
+    NSString *companyURL = [DBAPIClient baseUrl];
+    NSString *namespace = [companyURL componentsSeparatedByString:@"."][0];
+    namespace = [namespace stringByReplacingOccurrencesOfString:@"http://" withString:@""];
+    
+    NSMutableString *companyMail = [NSMutableString new];
+    [companyMail appendString:@"support+"];
+    [companyMail appendString:namespace];
+    [companyMail appendString:@"@ru-beacon.ru"];
+    if ([MFMailComposeViewController canSendMail]) {
         MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
         [mailer setSubject:NSLocalizedString(@"Обратная связь", nil)];
-        [mailer setToRecipients:@[@"team@empatika.com", @"team@ru-beacon.ru"]];
+        [mailer setToRecipients:@[companyURL, @"team@ru-beacon.ru"]];
         if(recipients)
             [mailer setToRecipients:recipients];
         [mailer setMailComposeDelegate:self];
