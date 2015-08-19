777146870bc47a892a13dbcd68a05cb4cc182ba2
Ivan Oschepkov <isoschepkov@gmail.com>
2015-08-19 19:49:17 +0400

    .

---
+++ DoubleB/ViewControllers/Payment/Modules/Cash/DBPaymentCashModuleView.m
@@ -0,0 +1,21 @@
+//
+//  DBPaymentCashModuleView.m
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 19.08.15.
+//  Copyright (c) 2015 Empatika. All rights reserved.
+//
+
+#import "DBPaymentCashModuleView.h"
+
+@implementation DBPaymentCashModuleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
