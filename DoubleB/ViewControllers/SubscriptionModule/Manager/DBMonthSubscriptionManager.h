06392bf4042c003587e9bdb8d203b0b27aa80245
Ivan Oschepkov <isoschepkov@gmail.com>
2015-09-14 18:57:43 +0300

    .

--- DoubleB/ViewControllers/SubscriptionModule/Manager/DBMonthSubscriptionManager.h
+++ DoubleB/ViewControllers/SubscriptionModule/Manager/DBMonthSubscriptionManager.h
@@ -11,6 +11,19 @@
 
 @class DBPaymentCard;
 
+@interface DBMonthSubscriptionVariant : NSObject<NSCoding>
+
+@property (strong, nonatomic) NSString *variantId;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *variantDescription;
+@property (nonatomic) NSInteger count;
+@property (nonatomic) double price;
+@property (nonatomic) NSInteger period;
+
+- (instancetype)initWithResponseDict:(NSDictionary *)dict;
+
+@end
+
 @interface DBMonthSubscriptionManager : DBPrimaryManager
 
 @property (strong, nonatomic) NSString *title;
@@ -18,8 +31,16 @@
 @property (strong, nonatomic) NSString *subscriptionScreenTitle;
 @property (strong, nonatomic) NSString *subscriptionScreenText;
 
+@property (strong, nonatomic) NSArray *subscriptionVariants;
 @property (nonatomic) NSInteger *balance;
+@property (strong, nonatomic) DBMenuPosition *positon;
 
-- (void)buySubscription:(void(^)(BOOL success, NSString *errorMessage))callback;
+- (void)synchWithResponseInfo:(NSDictionary *)infoDict;
+
+- (void)buySubscription:(DBMonthSubscriptionVariant *)variant
+               callback:(void(^)(BOOL success, NSString *errorMessage))callback;
+
+- (void)checkSubscriptionVariants:(void(^)(NSArray *variants))success
+                          failure:(void(^)(NSString *errorMessage))failure;
 
 @end
