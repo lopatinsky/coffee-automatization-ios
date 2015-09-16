c1a5aa5050831c9542006ac1e8274b86df5b01c2
yabalaban <balaban.alexander@gmail.com>
2015-09-16 22:47:29 +0300

    fix

--- DoubleB/ViewControllers/DBNewOrderViewController.m
+++ DoubleB/ViewControllers/DBNewOrderViewController.m
@@ -724,7 +724,8 @@ NSString *const kDBDefaultsFaves = @"kDBDefaultsFaves";
                 if (location) {
                     [[NetworkManager sharedManager] addPendingOperation:NetworkOperationFetchVenues withUserInfo:@{@"location": location}];
                 } else {
-                    [self setVenue:[[Venue storedVenues] firstObject]];
+//                    [self setVenue:[[Venue storedVenues] firstObject]];
+                    [self updateVenuesStatus];
                 }
                 
                 [self.orderFooter.activityIndicator stopAnimating];
