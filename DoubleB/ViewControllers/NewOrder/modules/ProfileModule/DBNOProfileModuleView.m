5cfbfa0457a807a3240d8fc8e7dd4d0fdee88d30
Ivan Oschepkov <isoschepkov@gmail.com>
2015-10-15 21:30:15 +0300

    profile module

--- DoubleB/ViewControllers/NewOrder/modules/ProfileModule/DBNOProfileModuleView.m
+++ DoubleB/ViewControllers/NewOrder/modules/ProfileModule/DBNOProfileModuleView.m
@@ -7,6 +7,9 @@
 //
 
 #import "DBNOProfileModuleView.h"
+#import "DBClientInfo.h"
+
+#import "DBProfileViewController.h"
 
 @interface DBNOProfileModuleView ()
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
@@ -24,10 +27,38 @@
 
 - (void)awakeFromNib {
     [self.profileImageView templateImageWithName:@"profile"];
+    
+    [[DBClientInfo sharedInstance] addObserver:self withKeyPaths:@[DBClientInfoNotificationClientName, DBClientInfoNotificationClientPhone, DBClientInfoNotificationClientMail] selector:@selector(reload)];
+}
+
+- (void)dealloc {
+    [[DBClientInfo sharedInstance] removeObserver:self];
 }
 
 - (void)reload:(BOOL)animated {
     [super reload:animated];
+    
+    if ([DBClientInfo sharedInstance].clientName.valid) {
+        if (![DBClientInfo sharedInstance].clientPhone.valid) {
+            self.titleLabel.text = NSLocalizedString(@"Укажите, пожалуйста, номер телефона", nil);
+            self.titleLabel.textColor = [UIColor db_errorColor];
+        } else {
+            self.titleLabel.text = [DBClientInfo sharedInstance].clientName.value;
+            self.titleLabel.textColor = [UIColor blackColor];
+        }
+    } else {
+        self.titleLabel.text = NSLocalizedString(@"Представьтесь, пожалуйста", nil);
+        self.titleLabel.textColor = [UIColor db_errorColor];
+    }
+}
+
+- (void)touchAtLocation:(CGPoint)location {
+    [GANHelper analyzeEvent:@"profile_click" category:self.analyticsCategory];
+
+    DBProfileViewController *profileViewController = [DBProfileViewController new];
+    profileViewController.hidesBottomBarWhenPushed = YES;
+    profileViewController.analyticsScreen = @"Profile_order_screen";
+    [self.ownerViewController.navigationController pushViewController:profileViewController animated:YES];
 }
 
 @end
