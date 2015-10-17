b5cae212177ca52b19e8a3be74d72f0c645faf3d
Ivan Oschepkov <isoschepkov@gmail.com>
2015-10-17 20:16:16 +0300

    start filling new order screen

--- DoubleB/ViewControllers/NewOrder/DBNewOrderVC.m
+++ DoubleB/ViewControllers/NewOrder/DBNewOrderVC.m
@@ -7,31 +7,78 @@
 //
 
 #import "DBNewOrderVC.h"
+#import "DBSettingsTableViewController.h"
+
+#import "DBNOItemsModuleView.h"
+#import "DBNOGiftItemsModuleView.h"
+#import "DBNOBonusItemsModuleView.h"
+#import "DBNOWalletModuleView.h"
+#import "DBNOTotalModuleView.h"
+#import "DBNOPromosModuleView.h"
+#import "DBNOVenueModelView.h"
+#import "DBNOTimeModuleView.h"
+#import "DBNOProfileModuleView.h"
+#import "DBNOPaymentModuleView.h"
+#import "DBNOCommentModuleView.h"
+#import "DBNOndaModuleView.h"
+#import "DBNOOrderModuleView.h"
 
 @interface DBNewOrderVC ()
-
+@property (strong, nonatomic) NSString *analyticsCategory;
 @end
 
 @implementation DBNewOrderVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+    self.view.backgroundColor = [UIColor db_backgroundColor];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.edgesForExtendedLayout = UIRectEdgeTop;
+    [self db_setTitle:NSLocalizedString(@"Заказ", nil)];
+    
+    self.analyticsCategory = ORDER_SCREEN;
+    
+    [self setupSettingsNavigationItem];
+    [self setupModules];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [Compatibility registerForNotifications];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [GANHelper analyzeScreen:self.analyticsCategory];
 }
-*/
+
+- (void)setupModules {
+    DBNOItemsModuleView *itemsModule = [DBNOItemsModuleView new];
+    itemsModule.analyticsCategory = self.analyticsCategory;
+    itemsModule.ownerViewController = self;
+    [self.modules addObject:itemsModule];
+    
+    DBNOBonusItemsModuleView *bonusItemsModule = [DBNOBonusItemsModuleView new];
+    bonusItemsModule.analyticsCategory = self.analyticsCategory;
+    bonusItemsModule.ownerViewController = self;
+    [self.modules addObject:bonusItemsModule];
+}
+
+- (void)setupSettingsNavigationItem{
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"settings.png"]
+                                                                             style:UIBarButtonItemStylePlain
+                                                                            target:self
+                                                                            action:@selector(clickSettings:)];
+}
+
+- (void)clickSettings:(id)sender {
+    DBSettingsTableViewController *settingsController = [DBClassLoader loadSettingsViewController];
+    settingsController.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:settingsController animated:YES];
+}
 
 @end
