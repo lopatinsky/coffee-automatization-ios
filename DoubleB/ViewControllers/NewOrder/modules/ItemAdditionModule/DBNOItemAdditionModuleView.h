75a1d9b7ef78d9f6a5e5933c7b56b5ca304c204d
Ivan Oschepkov <isoschepkov@gmail.com>
2015-10-19 18:25:00 +0300

    item addition module

---
+++ DoubleB/ViewControllers/NewOrder/modules/ItemAdditionModule/DBNOItemAdditionModuleView.h
@@ -0,0 +1,13 @@
+//
+//  DBNOItemAdditionModuleView.h
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 19/10/15.
+//  Copyright © 2015 Empatika. All rights reserved.
+//
+
+#import "DBModuleView.h"
+
+@interface DBNOItemAdditionModuleView : DBModuleView
+
+@end
---
+++ DoubleB/ViewControllers/NewOrder/modules/ItemAdditionModule/DBNOItemAdditionModuleView.m
@@ -0,0 +1,21 @@
+//
+//  DBNOItemAdditionModuleView.m
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 19/10/15.
+//  Copyright © 2015 Empatika. All rights reserved.
+//
+
+#import "DBNOItemAdditionModuleView.h"
+
+@implementation DBNOItemAdditionModuleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
