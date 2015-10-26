614fe5721cd9fd58a687a35119fce755e90f73e2
yabalaban <balaban.alexander@gmail.com>
2015-10-26 22:29:27 +0300

    card_add_click event

--- DoubleB/Helpers/FriendGift/ViewControllers/FriendGiftVC/Modules/CardModule/DBFGPaymentModule.m
+++ DoubleB/Helpers/FriendGift/ViewControllers/FriendGiftVC/Modules/CardModule/DBFGPaymentModule.m
@@ -54,7 +54,7 @@
 }
 
 - (void)touchAtLocation:(CGPoint)location {
-    [GANHelper analyzeEvent:@"gift_card_choice_click" category:self.analyticsCategory];
+    [GANHelper analyzeEvent:@"card_add_click" category:self.analyticsCategory];
     
     DBPaymentViewController *paymentVC = [DBPaymentViewController new];
     paymentVC.mode = DBPaymentViewControllerModeChoosePayment;
