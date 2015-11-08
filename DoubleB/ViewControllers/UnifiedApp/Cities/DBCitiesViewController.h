24ce63b894c24d6f2d6488812db016442dfabb39
Ivan Oschepkov <isoschepkov@gmail.com>
2015-11-08 13:10:01 +0300

    Unified app initial

---
+++ DoubleB/ViewControllers/UnifiedApp/Cities/DBCitiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DBCitiesViewController.h
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 08/11/15.
+//  Copyright © 2015 Empatika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBCitiesViewController : UIViewController
+
+@end
---
+++ DoubleB/ViewControllers/UnifiedApp/Cities/DBCitiesViewController.m
@@ -0,0 +1,28 @@
+//
+//  DBCitiesViewController.m
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 08/11/15.
+//  Copyright © 2015 Empatika. All rights reserved.
+//
+
+#import "DBCitiesViewController.h"
+
+@interface DBCitiesViewController ()
+
+@end
+
+@implementation DBCitiesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
