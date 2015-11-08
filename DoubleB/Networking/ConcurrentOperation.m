d74615e1c7efdf46d2ce5ca035465b9e866e1afa
yabalaban <balaban.alexander@gmail.com>
2015-11-08 15:30:11 +0300

    .

--- DoubleB/Networking/ConcurrentOperation.m
+++ DoubleB/Networking/ConcurrentOperation.m
@@ -24,6 +24,12 @@ NSString *const kDBConcurrentOperationFetchVenuesFinished = @"kDBConcurrentOpera
 #pragma mark - FetchUnifiedCities Operation
 NSString * __nonnull const kDBConcurrentOperationUnifiedCitiesLoadSuccess = @"kDBConcurrentOperationUnifiedCitiesLoadSuccess";
 NSString * __nonnull const kDBConcurrentOperationUnifiedCitiesLoadFailure = @"kDBConcurrentOperationUnifiedCitiesLoadFailure";
+#pragma mark - FetchUnifiedVenues Operation
+NSString * __nonnull const kDBConcurrentOperationUnifiedVenuesLoadSuccess = @"kDBConcurrentOperationUnifiedVenuesLoadSuccess";
+NSString * __nonnull const kDBConcurrentOperationUnifiedVenuesLoadFailure = @"kDBConcurrentOperationUnifiedVenuesLoadFailure";
+#pragma mark - FetchUnifiedMenu Operation
+NSString * __nonnull const kDBConcurrentOperationUnifiedMenuLoadSuccess = @"kDBConcurrentOperationUnifiedMenuLoadSuccess";
+NSString * __nonnull const kDBConcurrentOperationUnifiedMenuLoadFailure = @"kDBConcurrentOperationUnifiedMenuLoadFailure";
 
 @implementation ConcurrentOperation
 
--- DoubleB/ViewControllers/UnifiedApp/Cities/DBCitiesViewController.m
+++ DoubleB/ViewControllers/UnifiedApp/Cities/DBCitiesViewController.m
@@ -27,7 +27,7 @@
 - (void)viewWillAppear:(BOOL)animated {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(citiesAreBecomeAvailable) name:kDBConcurrentOperationUnifiedCitiesLoadSuccess object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(citiesDownloadFailed) name:kDBConcurrentOperationUnifiedCitiesLoadFailure object:nil];
-    [[NetworkManager sharedManager] addPendingOperation:NetworkOperationFetchUnifiedCities];
+    [[NetworkManager sharedManager] addUniqueOperation:NetworkOperationFetchUnifiedCities];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,7 +36,7 @@
 }
 
 #pragma mark - Networking
-- (void)citiesAreBecomeAvailable {
+- (void)citiesAreAvailable {
     
 }
 
