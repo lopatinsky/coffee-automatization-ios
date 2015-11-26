9c14de15bb21f533da8b562556bf427d2ac2eae0
yabalaban <balaban.alexander@gmail.com>
2015-11-26 15:54:54 +0300

    SDWebImage -> PINRemoteImage

--- DoubleB/ViewControllers/Menu/Position/PositionViewController1.m
+++ DoubleB/ViewControllers/Menu/Position/PositionViewController1.m
@@ -19,7 +19,7 @@
 
 #import "UIView+RoundedCorners.h"
 #import "UINavigationController+DBAnimation.h"
-#import "UIImageView+WebCache.h"
+#import "UIImageView+PINRemoteImage.h"
 
 @interface PositionViewController1 ()<DBPositionModifierPickerDelegate, DBPositionModifiersListDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *positionImageView;
@@ -78,14 +78,14 @@
     // Configure position image
     self.positionImageView.backgroundColor = [UIColor colorWithRed:200./255 green:200./255 blue:200./255 alpha:0.3f];
     self.defaultPositionImageView.hidden = NO;
-    if(self.position.imageUrl){
-        [self.positionImageView sd_setImageWithURL:[NSURL URLWithString:self.position.imageUrl]
-                                         completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-                                             if(!error){
-                                                 self.positionImageView.backgroundColor = [UIColor clearColor];
-                                                 self.defaultPositionImageView.hidden = YES;
-                                             }
-                                         }];
+    if (self.position.imageUrl) {
+        [self.positionImageView setPin_updateWithProgress:YES];
+        [self.positionImageView pin_setImageFromURL:[NSURL URLWithString:self.position.imageUrl] completion:^(PINRemoteImageManagerResult *result) {
+            if (result.resultType != PINRemoteImageResultTypeNone) {
+                self.positionImageView.backgroundColor = [UIColor clearColor];
+                self.defaultPositionImageView.hidden = YES;
+            }
+        }];
     }
     
     self.positionTitleLabel.text = self.position.name;
