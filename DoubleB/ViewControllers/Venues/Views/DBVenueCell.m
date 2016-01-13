40ebadacd265908bf85ca54e3e341d2de8605466
Ivan Oschepkov <isoschepkov@gmail.com>
2016-01-13 16:43:00 +0300

    refactoring venues screen

---
+++ DoubleB/ViewControllers/Venues/Views/DBVenueCell.m
@@ -0,0 +1,63 @@
+//
+//  DBVenueCell.m
+//  DoubleB
+//
+//  Created by Ivan Oschepkov on 13.10.14.
+//  Copyright (c) 2014 Empatika. All rights reserved.
+//
+
+#import "DBVenueCell.h"
+#import "Venue.h"
+#import "LocationHelper.h"
+
+@interface DBVenueCell () <UIGestureRecognizerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *moreInfoView;
+@property (weak, nonatomic) IBOutlet UIButton *moreInfoButton;
+@property (weak, nonatomic) IBOutlet UIImageView *infoImageView;
+@end
+
+@implementation DBVenueCell
+
+- (void)awakeFromNib {
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    [self.venueDistanceLabel.layer setCornerRadius:5];
+    [self.moreInfoButton addTarget:self action:@selector(moreInfoButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.infoImageView templateImageWithName:@"info_icon"];
+}
+
+- (void)moreInfoButtonClick {
+    if ([self.delegate respondsToSelector:@selector(db_venueCellDidSelectInfo:)]) {
+        [self.delegate db_venueCellDidSelectInfo:self];
+    }
+}
+
+- (void)configure:(Venue *)venue {
+    _venue = venue;
+    
+    double dist = venue.distance;
+    if (dist && dist > 0) {
+        [self.venueDistanceLabel setBackgroundColor:[UIColor db_defaultColor]];
+        if (dist > 1) {
+            self.venueDistanceLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%.1f км", nil), dist];
+            if (dist > 3) {
+                [self.venueDistanceLabel setBackgroundColor:[UIColor fromHex:0xffa1aaaa]];
+            }
+        } else {
+            self.venueDistanceLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%.0f м", nil), dist * 1000];
+        }
+        self.venueDistanceLabel.hidden = NO;
+        self.constraintDistanceLabelWidth.constant = 55;
+        self.constraintDistanceLabelAndVenueNameLabelSpace.constant = 10;
+    } else {
+        self.venueDistanceLabel.hidden = YES;
+        self.constraintDistanceLabelWidth.constant = 0;
+        self.constraintDistanceLabelAndVenueNameLabelSpace.constant = 0;
+    }
+    self.venueNameLabel.text = venue.title;
+    self.venueAddressLabel.text = venue.address;
+    self.venueWorkTimeLabel.text = venue.workingTime ?: NSLocalizedString(@"Пн-пт 8:00-20:00, сб-вс 11:00-18:00", nil);
+}
+
+@end
