4f18f8f6ede15eb4203a355c2962e3587614575e
Ivan Oschepkov <isoschepkov@gmail.com>
2016-01-13 16:32:44 +0300

    share after order from server

--- DoubleB/ViewControllers/NewOrder/modules/OrderModule/DBNOOrderModuleView.m
+++ DoubleB/ViewControllers/NewOrder/modules/OrderModule/DBNOOrderModuleView.m
@@ -123,7 +123,7 @@
     [GANHelper analyzeEvent:@"order_button_click" category:ORDER_SCREEN];
     
     [MBProgressHUD showHUDAddedTo:self.ownerViewController.view animated:YES];
-    [DBServerAPI createNewOrder:^(Order *order) {
+    [DBServerAPI createNewOrder:^(Order *order, NSDictionary *response) {
         [MBProgressHUD hideHUDForView:self.ownerViewController.view animated:YES];
         
         [[ApplicationManager sharedInstance] moveToScreen:ApplicationScreenHistoryOrder object:order animated:YES];
@@ -138,7 +138,7 @@
                              otherButtonTitles:nil
                                        handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
                                            // Show suggestion to share
-                                           if ([DBShareHelper sharedInstance].shareSuggestionIsAvailable) {
+                                           if ([DBShareHelper sharedInstance].shareSuggestionIsAvailable && [response[@"show_share"] boolValue]) {
                                                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                                                    [[DBShareHelper sharedInstance] showShareSuggestion:YES];
                                                });
